e47de936674499a786b2c3e97f2238d4d9311790
Mohammad M Alam <malam004@gmail.com>
2022-02-24 11:26:20 -0600

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,9 +3,19 @@ import React from "react";
 import "./Button.css";
 
 const Button = ({ fColor, className, value, onClick }) => {
+  let bColor;
+  if (
+    value === "X" ||
+    value === "/" ||
+    value === "+" ||
+    value === "-" ||
+    value === "="
+  )
+    bColor = "#F57F17";
+  else bColor = "#522a9e";
   return (
     <IonButton
-      style={{ color: fColor }}
+      style={{ color: fColor, "--background": bColor }}
       className={className}
       onClick={onClick}
     >
